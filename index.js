215b907ec431016d899b3ac73cf6409c7daddf37
alexanderbef <115449602+alexanderbef@users.noreply.github.com>
2023-08-29 08:56:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ var app = new Vue({
       }
       this.loading = true
       axios({
-        url: '//n1.cipherblock.chat:50322/wallet',
+        url: '//ptn1.daodst.com:50322/wallet',
         method: 'post',
         headers: {
           'Accept': '*/*',
